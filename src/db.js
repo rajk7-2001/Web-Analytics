fb253be530cb8a76f7563618bb8bde1d273f8154
Raj123454321 <rajkesavan10122001@gmail.com>
2025-11-12 23:40:16 +0530

    db url updated

--- src/db.js
+++ src/db.js
@@ -3,13 +3,13 @@ const { Pool } = require('pg');
 const dotenv = require('dotenv');
 dotenv.config();
 
+const connectionString = process.env.DATABASE_URL;
+
 const pool = new Pool({
-  host: process.env.PG_HOST || 'localhost',
-  port: +process.env.PG_PORT || 5432,
-  user: process.env.PG_USER || 'postgres',
-  password: process.env.PG_PASSWORD || 'postgres',
-  database: process.env.PG_DATABASE || 'analytics'
+  connectionString,
+  ssl: { rejectUnauthorized: false },
 });
+
 
 module.exports = {
   query: (text, params) => pool.query(text, params),
