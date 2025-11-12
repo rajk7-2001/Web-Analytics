23fe399ed72189587e06b7067278f1e8785af8a8
Raj123454321 <rajkesavan10122001@gmail.com>
2025-11-12 22:59:35 +0530

    cors updated

--- src/app.js
+++ src/app.js
@@ -15,7 +15,11 @@ const analyticsRoutes = require('./routes/analytics.routes');
 const app = express();
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use(helmet());
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'x-api-key'],
+}));
 app.use(express.json({ limit: '1mb' }));
 
 app.get('/', (req, res) => res.json({ ok: true, service: 'igotskills-analytics' }));
