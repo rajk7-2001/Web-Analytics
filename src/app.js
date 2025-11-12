7713556eec0d84c5afe2e6d2e8a3e22856e55ce9
Raj123454321 <rajkesavan10122001@gmail.com>
2025-11-12 23:15:53 +0530

    updated app.js code for cors

--- src/app.js
+++ src/app.js
@@ -5,24 +5,27 @@ const helmet = require('helmet');
 const dotenv = require('dotenv');
 const swaggerUi = require('swagger-ui-express');
 const swaggerSpec = require('./swagger');
-
-
 dotenv.config();
+
+const app = express();
+
+app.use(helmet());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'x-api-key'],
+}));
+
+app.options('*', cors());
+app.use(express.json({ limit: '1mb' }));
+
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+app.get('/', (req, res) => res.json({ ok: true, service: 'igotskills-analytics' }));
 
 const authRoutes = require('./routes/auth.routes');
 const analyticsRoutes = require('./routes/analytics.routes');
 
-const app = express();
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-app.use(helmet());
-app.use(cors({
-  origin: ['https://analytics-backend-xz2s.onrender.com'],
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'x-api-key'],
-}));
-app.use(express.json({ limit: '1mb' }));
-
-app.get('/', (req, res) => res.json({ ok: true, service: 'igotskills-analytics' }));
 
 app.use('/api/auth', authRoutes);
 app.use('/api/analytics', analyticsRoutes);
